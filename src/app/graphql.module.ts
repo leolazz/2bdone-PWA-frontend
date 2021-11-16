ed4c3b595ea6699e22ae412819424dadd6124e92
Leo Lazzarini <leolazzarini101@gmail.com>
2021-11-16 15:49:46 -0800

    Error:this.cache.batch - fixed v2 migration guide

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,33 +1,24 @@
-import { NgModule } from '@angular/core';
-import { APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClientOptions } from 'apollo-client';
-import { from } from 'apollo-link';
 import { environment } from '../environments/environment';
-
-// this is here to access the cache directly
-export const cache = new InMemoryCache();
-
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  const apolloLink = httpLink.create({
-    uri: `${environment.serverUrl}graphql`,
-    withCredentials: true,
-  });
-
-  return {
-    link: from([apolloLink]),
-    cache,
-    connectToDevTools: true,
-  } as ApolloClientOptions<any>;
-}
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 
 @NgModule({
-  exports: [HttpLinkModule],
+  imports: [HttpClientModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: `${environment.serverUrl}graphql`,
+            withCredentials: true,
+          }),
+        };
+      },
       deps: [HttpLink],
     },
   ],
