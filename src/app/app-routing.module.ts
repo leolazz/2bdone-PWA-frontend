5daed6987ae07eb564b11c5073853d779d26e41c
leolazz <leolazzarini101@gmail.com>
2022-02-25 11:33:05 -0800

    Updated routing paths & added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { IntroGuard } from './guards/intro.guard';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+    canLoad: [AuthGuard],
   },
-  {
-    path: 'add-task',
-    loadChildren: () =>
-      import('./pages/add-task/add-task.module').then(
-        (m) => m.AddTaskPageModule
-      ),
-  },
-  {
-    path: 'add-project',
-    loadChildren: () =>
-      import('./pages/add-project/add-project.module').then(
-        (m) => m.AddProjectPageModule
-      ),
-  },
+
   {
     path: 'intro',
-    loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () =>
+      import('./pages/intro/intro.module').then((m) => m.IntroPageModule),
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    canLoad: [IntroGuard, AutoLoginGuard],
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
 ];
 @NgModule({
